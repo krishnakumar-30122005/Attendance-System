d60598777bb01a2ba8cc647e77aa316c761b9420
Krishna Kumar <149188795+krishnakumar-30122005@users.noreply.github.com>
2025-09-05 22:31:07 +0530

    Update basics.py

--- Facial_Recognition/basics.py
+++ Facial_Recognition/basics.py
@@ -1,11 +1,4 @@
-import cv2
-import face_recognition
- 
-imgElon = face_recognition.load_image_file('ImagesBasic/Elon Musk.jpg')
-imgElon = cv2.cvtColor(imgElon,cv2.COLOR_BGR2RGB)
-imgTest = face_recognition.load_image_file('ImagesBasic/Bill gates.jpg')
-imgTest = cv2.cvtColor(imgTest,cv2.COLOR_BGR2RGB)
- 
+
 faceLoc = face_recognition.face_locations(imgElon)[0]
 encodeElon = face_recognition.face_encodings(imgElon)[0]
 cv2.rectangle(imgElon,(faceLoc[3],faceLoc[0]),(faceLoc[1],faceLoc[2]),(255,0,255),2)
